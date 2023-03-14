533af6058983f9cc1f8a302b1043ecb4405e019b
michaelboegner <cmikeb@gmail.com>
2023-03-14 11:53:02 -0400

    Successful attempt

---
+++ Diag-Sums-HackerRank.js
@@ -0,0 +1,80 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'diagonalDifference' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+ */
+
+function diagonalDifference(arr) {
+    let rightDiag = [];
+    let leftDiag = [];
+    
+    
+    console.log(arr, "arr");
+    for(let i = 0; i < arr.length; i++) {
+        leftDiag.push(arr[i][i]); 
+    }
+    
+    let arrReversed = arr.reverse();
+    
+    for(let i = 0; i < arr.length; i++) {
+        rightDiag.push(arrReversed[i][i]);
+    }
+         
+    console.log(arrReversed, "arrReversed");
+    
+    console.log(rightDiag, "rightDiag");
+    console.log(leftDiag, "leftDiag");
+    let sumRight = 0;
+    let sumLeft = 0;
+    
+    for(let i = 0; i < rightDiag.length; i++) {
+        sumRight += rightDiag[i];
+        sumLeft += leftDiag[i];
+    }
+    
+    let final = Math.abs(sumRight - sumLeft);
+    return final;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    let arr = Array(n);
+
+    for (let i = 0; i < n; i++) {
+        arr[i] = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+    }
+
+    const result = diagonalDifference(arr);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
